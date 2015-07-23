89aef2defc9e3a5105a0c21864bdbc805c8f07f9
tivaliy <vitaliy@kulanov.org.ua>
2015-07-23 14:26:16 +0300

    Add restricted_prime.py solution

---
+++ restricted_prime.py
@@ -0,0 +1,19 @@
+# http://www.checkio.org/mission/restricted-prime/
+__author__ = 'Vitalii K'
+
+
+def checkio(number):
+    i = ord(' ') ^ ord('"')
+    while i < number:
+        curr_number = number
+        while curr_number >= (ord('a')) ^ (ord('a')):
+            i_neg = ~i + (ord(' ') ^ ord('!'))
+            curr_number += i_neg
+            # print(curr_number)
+            if curr_number == (ord('a')) ^ (ord('a')):
+                return False
+        i += ord(' ') ^ ord('!')
+    return True
+
+
+
