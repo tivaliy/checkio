efc686070efb778539a3f636459d7cc4eb90f62a
tivaliy <vitaliy@kulanov.org.ua>
2015-07-31 13:34:08 +0300

    Update ghost_detect.py

--- ghost_detect.py
+++ ghost_detect.py
@@ -6,12 +6,3 @@ def recognize(n):
     # l = filter(None, "{0:b}".format(n).split('0'))
     l = list(filter(None, bin(n)[2:].split('0')))
     return l[1:] == l[:-1]
-
-
-print(recognize(21))
-
-print(recognize(1587))
-
-print(recognize(3687))
-
-print(recognize(1057222687))
