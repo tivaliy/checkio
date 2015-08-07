ce59b0941e81b3adfe8cde2587f1adb3447366a0
tivaliy <vitaliy@kulanov.org.ua>
2015-08-07 15:44:07 +0300

    Add polygon_area.py solution

---
+++ polygon_area.py
@@ -0,0 +1,26 @@
+# http://www.checkio.org/mission/area-of-a-convex-polygon/
+__author__ = 'Vitalii K'
+
+
+def checkio(data):
+    data.append(data[0])
+    s1, s2 = 0, 0
+    for i in range(len(data) - 2, -1, -1):
+        s1 += data[i][0] * data[i+1][1]
+        s2 += data[i][1] * data[i+1][0]
+    return abs((s2 - s1) / 2)
+
+
+if __name__ == '__main__':
+    #This part is using only for self-checking and not necessary for auto-testing
+    def almost_equal(checked, correct, significant_digits=1):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+
+    assert almost_equal(checkio([[1, 1], [9, 9], [9, 1]]), 32), "The half of the square"
+    assert almost_equal(checkio([[4, 10], [7, 1], [1, 4]]), 22.5), "Triangle"
+    assert almost_equal(checkio([[1, 2], [3, 8], [9, 8], [7, 1]]), 40), "Quadrilateral"
+    assert almost_equal(checkio([[3, 3], [2, 7], [5, 9], [8, 7], [7, 3]]), 26), "Pentagon"
+    assert almost_equal(checkio([[7, 2], [3, 2], [1, 5], [3, 9], [7, 9], [9, 6]]), 42), "Hexagon"
+    assert almost_equal(checkio([[4, 1], [3, 4], [3, 7], [4, 8], [7, 9], [9, 6], [7, 1]]), 35.5), "Heptagon"
+
