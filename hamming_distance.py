c05625cd953d8ae48ca025aaab5500cfb9521383
tivaliy <vitaliy@kulanov.org.ua>
2015-08-16 13:20:48 +0300

    Add hamming_distance.py solution

---
+++ hamming_distance.py
@@ -0,0 +1,12 @@
+# http://www.checkio.org/mission/hamming-distance2/
+__author__ = 'Vitalii K'
+
+
+def checkio(n, m):
+    return repr(bin(n ^ m)).count('1')
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
