4212d3d39c5b2a2aea531a291fb014e3d5c5be19
tivaliy <vitaliy@kulanov.org.ua>
2015-08-16 12:43:20 +0300

    Add count_inversions.py solution

---
+++ count_inversions.py
@@ -0,0 +1,15 @@
+# http://www.checkio.org/mission/count-inversions/
+__author__ = 'Vitalii K'
+
+import itertools
+
+
+def count_inversion(sequence):
+    return sum(a > b for a, b in itertools.combinations(sequence, r=2))
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_inversion((1, 2, 5, 3, 4, 7, 6)) == 3, "Example"
+    assert count_inversion((0, 1, 2, 3)) == 0, "Sorted"
+    assert count_inversion((99, -99)) == 1, "Two numbers"
+    assert count_inversion((5, 3, 2, 1, 0)) == 10, "Reversed"
