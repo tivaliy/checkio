935ee5cd18de2d3fe2fa96bfcd773f0c87676b51
tivaliy <vitaliy@kulanov.org.ua>
2015-08-18 22:38:03 +0300

    Uodate counting_tiles.py and weekend_counter.py files

--- counting_tiles.py
+++ counting_tiles.py
@@ -19,11 +19,9 @@ def checkio(radius):
         partial += floor(ceil(down_border))
     return [4*solid, 4*partial]
 
-print(checkio(2.5))
-
-# These "asserts" using only for self-checking and not necessary for auto-testing
-# if __name__ == '__main__':
-#     assert checkio(2) == [4, 12], "N=2"
-#     assert checkio(3) == [16, 20], "N=3"
-#     assert checkio(2.1) == [4, 20], "N=2.1"
-#     assert checkio(2.5) == [12, 20], "N=2.5"
+These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(2) == [4, 12], "N=2"
+    assert checkio(3) == [16, 20], "N=3"
+    assert checkio(2.1) == [4, 20], "N=2.1"
+    assert checkio(2.5) == [12, 20], "N=2.5"
--- weekend_counter.py
+++ weekend_counter.py
@@ -14,10 +14,6 @@ def checkio(from_date, to_date):
         from_date += datetime.timedelta(days=1)
     return weekdays_count
 
-
-print(checkio(date(2013, 2, 2), date(2013, 2, 3)))
-
-
 # These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(date(2013, 9, 18), date(2013, 9, 23)) == 2, "1st example"
