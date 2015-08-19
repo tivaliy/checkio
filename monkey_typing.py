bac57cfbb1dde2a53c8120baacdbc8fed41f0a75
tivaliy <vitaliy@kulanov.org.ua>
2015-08-19 08:53:24 +0300

    Add monkey_typing.py solution

---
+++ monkey_typing.py
@@ -0,0 +1,12 @@
+__author__ = 'Vitalii K'
+
+
+def count_words(text, words):
+    return len([w for w in words if w in text.lower()])
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_words("How aresjfhdskfhskd you?", {"how", "are", "you", "hello"}) == 3, "Example"
+    assert count_words("Bananas, give me bananas!!!", {"banana", "bananas"}) == 2, "BANANAS!"
+    assert count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                       {"sum", "hamlet", "infinity", "anything"}) == 1, "Weird text"
