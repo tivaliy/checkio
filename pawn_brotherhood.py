355a60e0d18423c9c25dca2c6b429ba6069a5315
tivaliy <vitaliy@kulanov.org.ua>
2015-08-24 22:10:35 +0300

    Add pawn_brotherhood.py solution

---
+++ pawn_brotherhood.py
@@ -0,0 +1,19 @@
+# http://www.checkio.org/mission/pawn-brotherhood/
+__author__ = 'Vitalii K'
+
+
+def safe_pawns(pawns):
+    safe_pawns_cnt = 0
+    for coord in pawns:
+        c_list = list(coord)
+        first_check = (chr(ord(c_list[0]) - 1) + str((int(c_list[1]) - 1)))
+        second_check = (chr(ord(c_list[0]) + 1) + str((int(c_list[1]) - 1)))
+        if first_check in pawns or second_check in pawns:
+            safe_pawns_cnt += 1
+    return safe_pawns_cnt
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
+    assert safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1
