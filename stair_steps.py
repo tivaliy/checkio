8180e106e751ebe566a542e071a7de97d61da050
tivaliy <vitaliy@kulanov.org.ua>
2016-01-04 14:50:18 +0200

    Add stair_steps.py solution

---
+++ stair_steps.py
@@ -0,0 +1,15 @@
+def checkio(numbers):
+    jump_one, jump_two = 0, 0
+    for n in numbers:
+        print(jump_two, jump_one)
+        jump_two, jump_one = jump_one, max(jump_one, jump_two) + n
+    return max(jump_one, jump_two)
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([5, -3, -1, 2]) == 6, 'Fifth'
+    assert checkio([5, 6, -10, -7, 4]) == 8, 'First'
+    assert checkio([-11, 69, 77, -51, 23, 67, 35, 27, -25, 95]) == 393, 'Second'
+    assert checkio([-21, -23, -69, -67, 1, 41, 97, 49, 27]) == 125, 'Third'
+    print('All ok')
