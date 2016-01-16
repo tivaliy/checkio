de9b3dc12afd56b013ff423824125763496d1e8e
tivaliy <vitaliy@kulanov.org.ua>
2016-01-16 13:30:36 +0200

    Update check_command.py

--- check_command.py
+++ check_command.py
@@ -1,4 +1,5 @@
-__author__ = 'Vitalii K'
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
 
 
 def check_command(pattern, command):
@@ -6,4 +7,13 @@ def check_command(pattern, command):
     bin_command = ''.join(['1' if x.isalpha() else '0' for x in command])
     return bin_str == bin_command
 
-print(check_command(42, "C2H5OH"))
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_command(42, "12a0b3e4") == True, "42 is the answer"
+    assert check_command(101, "ab23b4zz") == False, "one hundred plus one"
+    assert check_command(0, "478103487120470129") == True, "Any number"
+    assert check_command(127, "Checkio") == True, "Uppercase"
+    assert check_command(7, "Hello") == False, "Only full match"
+    assert check_command(8, "a") == False, "Too short command"
+    assert check_command(5, "H2O") == True, "Water"
+    assert check_command(42, "C2H5OH") == False, "Yep, this is not the Answer"
